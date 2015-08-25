314d960a90daefef67e3fc67814287f4ae6d4891
abhinay <abhinay212@gmail.com>
2015-08-25 06:11:44 -0700

    first commit

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php
+/**************************************************************************
+ *                                                                        *
+ *    4images - A Web Based Image Gallery Management System               *
+ *    ----------------------------------------------------------------    *
+ *                                                                        *
+ *             File: config.php                                           *
+ *        Copyright: (C) 2002 Jan Sorgalla                                *
+ *            Email: jan@4homepages.de                                    *
+ *              Web: http://www.4homepages.de                             *
+ *    Scriptversion: 1.7.11                                                *
+ *                                                                        *
+ *    Never released without support from: Nicky (http://www.nicky.net)   *
+ *                                                                        *
+ **************************************************************************
+ *                                                                        *
+ *    Dieses Script ist KEINE Freeware. Bitte lesen Sie die Lizenz-       *
+ *    bedingungen (Lizenz.txt) für weitere Informationen.                 *
+ *    ---------------------------------------------------------------     *
+ *    This script is NOT freeware! Please read the Copyright Notice       *
+ *    (Licence.txt) for further information.                              *
+ *                                                                        *
+ *************************************************************************/
+
+date_default_timezone_set("Asia/Calcutta");
+
+$db_servertype = 'mysql';
+$db_host = 'localhost';
+$db_name = 'images';
+$db_user = 'root';
+$db_password = 'password';
+
+$table_prefix = '4images_';
+
+define("4IMAGES_ACTIVE", 1);
+
+?>
